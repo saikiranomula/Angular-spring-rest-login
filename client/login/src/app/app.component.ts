23b2b5a8463360d50dd6ad4a8a581bb6a6d6f94d
Saikiran Nomula <33097603+saikiranomula@users.noreply.github.com>
2019-07-25 18:21:44 -0400

    log out success

--- client/login/src/app/app.component.ts
+++ client/login/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { User } from './user';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './_services';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App comp html';
+
+  currentUser: User;
+
+  constructor(
+      private router: Router,
+      private authenticationService: AuthenticationService
+  ) {
+      this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  logout() {
+      this.authenticationService.logout();
+      this.router.navigate(['/login']);
+  }
 }
